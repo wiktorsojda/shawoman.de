af6004d5f35f2bc876bf4b8799d53493addea20c
Wiktor Sojda <wiktor.sojda@gmail.com>
2026-08-06 13:27:09 +0200

    Update asset.php to invalidate browser cache for block editor scripts

--- build/glownagrid/index.asset.php
+++ build/glownagrid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'dc710a073421ececbaca');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '553841a214906d097e3a');
