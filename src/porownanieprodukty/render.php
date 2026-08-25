b3e3813308574a8344a5ab15b1b1a787217d7878
Wiktor Sojda <wiktor.sojda@gmail.com>
2026-08-25 14:50:42 +0200

    fix: wprowadzono natywne ikony SVG punktatorow dostarczone z figmy

--- src/porownanieprodukty/render.php
+++ src/porownanieprodukty/render.php
@@ -31,7 +31,15 @@ $features = !empty($a['features']) && is_array($a['features']) ? $a['features'] 
         <div class="porownanieprodukty__row">
           <!-- Desktop: Pokazuje nagłówek na środku. Mobile: Ukryte -->
           <div class="porownanieprodukty__feature porownanieprodukty__feature--left">
-            <span class="porownanieprodukty__dot porownanieprodukty__dot--green"></span>
+            <svg class="porownanieprodukty__dot" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <foreignObject x="-4" y="-4" width="26" height="26"><div xmlns="http://www.w3.org/1999/xhtml" style="backdrop-filter:blur(2px);clip-path:url(#bgblur_0_1774_2246_clip_path);height:100%;width:100%"></div></foreignObject><g data-figma-bg-blur-radius="4">
+              <rect width="18" height="18" rx="9" fill="#CFF2D6"/>
+              <rect x="4" y="4" width="10" height="10" rx="5" fill="#2D773D"/>
+              </g>
+              <defs>
+              <clipPath id="bgblur_0_1774_2246_clip_path" transform="translate(4 4)"><rect width="18" height="18" rx="9"/>
+              </clipPath></defs>
+            </svg>
             <span class="porownanieprodukty__text"><?php echo wp_kses_post($feature['left']); ?></span>
           </div>
           
@@ -40,7 +48,15 @@ $features = !empty($a['features']) && is_array($a['features']) ? $a['features'] 
           </div>
           
           <div class="porownanieprodukty__feature porownanieprodukty__feature--right">
-            <span class="porownanieprodukty__dot porownanieprodukty__dot--red"></span>
+            <svg class="porownanieprodukty__dot" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <foreignObject x="-4" y="-4" width="26" height="26"><div xmlns="http://www.w3.org/1999/xhtml" style="backdrop-filter:blur(2px);clip-path:url(#bgblur_0_1774_2264_clip_path);height:100%;width:100%"></div></foreignObject><g data-figma-bg-blur-radius="4">
+              <rect width="18" height="18" rx="9" fill="#FFC7C7"/>
+              <rect x="4" y="4" width="10" height="10" rx="5" fill="#AC0000"/>
+              </g>
+              <defs>
+              <clipPath id="bgblur_0_1774_2264_clip_path" transform="translate(4 4)"><rect width="18" height="18" rx="9"/>
+              </clipPath></defs>
+            </svg>
             <span class="porownanieprodukty__text"><?php echo wp_kses_post($feature['right']); ?></span>
           </div>
         </div>
