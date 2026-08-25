9f74a5a961de3a5d1c8029ab9395a20cc70d697c
Wiktor Sojda <wiktor.sojda@gmail.com>
2026-08-25 16:53:11 +0200

    chore: dodanie skompilowanych plików i najnowszych zmian

---
+++ build/jakuzywac/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '9e3df36b21201f210b36');
---
+++ build/jakuzywac/render.php
@@ -0,0 +1,41 @@
+<?php
+$a = $attributes;
+
+$title = !empty($a['title']) ? $a['title'] : "Wie funktioniert's?";
+$description = !empty($a['description']) ? $a['description'] : '';
+$steps = !empty($a['steps']) && is_array($a['steps']) ? $a['steps'] : [];
+?>
+<section <?php echo get_block_wrapper_attributes(['class' => 'jakuzywac']); ?>>
+  <div class="jakuzywac__inner">
+    <div class="jakuzywac__content">
+      <h2 class="jakuzywac__title"><?php echo wp_kses_post($title); ?></h2>
+      <div class="jakuzywac__desc">
+        <?php echo wp_kses_post($description); ?>
+      </div>
+    </div>
+    
+    <div class="jakuzywac__gallery">
+      <?php foreach ($steps as $index => $step) : ?>
+        <figure class="jakuzywac__step">
+          <div class="jakuzywac__image-wrapper">
+            <?php if (!empty($step['image']) || !empty($step['imageMobile'])) : ?>
+              <picture>
+                <?php if (!empty($step['imageMobile'])) : ?>
+                  <source media="(max-width: 767px)" srcset="<?php echo esc_url($step['imageMobile']); ?>" />
+                <?php endif; ?>
+                <?php if (!empty($step['image'])) : ?>
+                  <source media="(min-width: 768px)" srcset="<?php echo esc_url($step['image']); ?>" />
+                  <img src="<?php echo esc_url($step['image']); ?>" alt="Krok <?php echo $index + 1; ?>" class="jakuzywac__image" />
+                <?php elseif (!empty($step['imageMobile'])) : ?>
+                  <img src="<?php echo esc_url($step['imageMobile']); ?>" alt="Krok <?php echo $index + 1; ?>" class="jakuzywac__image" />
+                <?php endif; ?>
+              </picture>
+            <?php else : ?>
+              <div class="jakuzywac__image-placeholder"></div>
+            <?php endif; ?>
+          </div>
+        </figure>
+      <?php endforeach; ?>
+    </div>
+  </div>
+</section>
