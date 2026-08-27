8c9c723934949f775df9753f7ed4dd257afa7619
Wiktor Sojda <wiktor.sojda@gmail.com>
2026-08-27 10:41:22 +0200

    Translate Dostawa to Versand in cart-shipping

--- woocommerce/cart/cart-shipping.php
+++ woocommerce/cart/cart-shipping.php
@@ -24,10 +24,10 @@ $shipping_cost = WC()->cart->get_cart_shipping_total();
 
 ?>
 <tr class="woocommerce-shipping-totals shipping">
-	<th><?php esc_html_e( 'Dostawa', 'woocommerce' ); ?></th>
-	<td data-title="<?php esc_attr_e( 'Dostawa', 'woocommerce' ); ?>">
+	<th><?php esc_html_e( 'Versand', 'woocommerce' ); ?></th>
+	<td data-title="<?php esc_attr_e( 'Versand', 'woocommerce' ); ?>">
 		<?php if ( $shipping_cost === 'Free!' ) : ?>
-			<p><?php esc_html_e( 'Dostawa: Free', 'woocommerce' ); ?></p>
+			<p><?php esc_html_e( 'Kostenloser Versand', 'woocommerce' ); ?></p>
 		<?php else : ?>
 			<p><?php echo sprintf( esc_html__( '%s', 'woocommerce' ), $shipping_cost  ); ?></p>
 		<?php endif; ?>
