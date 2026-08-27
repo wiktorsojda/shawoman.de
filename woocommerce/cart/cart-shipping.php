12d49d39193acaeb3c2d5096680c4895ca9eebdb
Wiktor Sojda <wiktor.sojda@gmail.com>
2026-08-27 10:49:07 +0200

    Update Order Received styles for WooCommerce Blocks

--- woocommerce/cart/cart-shipping.php
+++ woocommerce/cart/cart-shipping.php
@@ -9,14 +9,14 @@
  * @version 8.8.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$has_calculated_shipping  = ! empty( $has_calculated_shipping );
-$show_shipping_calculator = ! empty( $show_shipping_calculator );
-$calculator_text          = '';
+$has_calculated_shipping = !empty($has_calculated_shipping);
+$show_shipping_calculator = !empty($show_shipping_calculator);
+$calculator_text = '';
 
 // Get the chosen shipping method
-$chosen_methods = WC()->session->get( 'chosen_shipping_methods' );
+$chosen_methods = WC()->session->get('chosen_shipping_methods');
 $chosen_shipping = $chosen_methods ? $chosen_methods[0] : '';
 
 // Get the shipping cost
@@ -24,13 +24,12 @@ $shipping_cost = WC()->cart->get_cart_shipping_total();
 
 ?>
 <tr class="woocommerce-shipping-totals shipping">
-	<th><?php esc_html_e( 'Versand', 'woocommerce' ); ?></th>
-	<td data-title="<?php esc_attr_e( 'Versand', 'woocommerce' ); ?>">
-		<?php if ( $shipping_cost === 'Free!' ) : ?>
-			<p><?php esc_html_e( 'Kostenloser Versand', 'woocommerce' ); ?></p>
-		<?php else : ?>
-			<p><?php echo sprintf( esc_html__( '%s', 'woocommerce' ), $shipping_cost  ); ?></p>
+	<th><?php esc_html_e('Dostawa', 'woocommerce'); ?></th>
+	<td data-title="<?php esc_attr_e('Dostawa', 'woocommerce'); ?>">
+		<?php if ($shipping_cost === 'Free!'): ?>
+			<p><?php esc_html_e('Dostawa: Free', 'woocommerce'); ?></p>
+		<?php else: ?>
+			<p><?php echo sprintf(esc_html__('%s', 'woocommerce'), $shipping_cost); ?></p>
 		<?php endif; ?>
 	</td>
 </tr>
-
