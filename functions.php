0b28c6f5f9083fb23cf058bca25800bedeab3df8
Wiktor Sojda <wiktor.sojda@gmail.com>
2026-09-02 15:23:09 +0200

    feat: add rating pill below product subtitle

--- functions.php
+++ functions.php
@@ -2200,6 +2200,29 @@ function display_product_subtitle()
 }
 add_action('woocommerce_single_product_summary', 'display_product_subtitle', 6);
 
+// Display rating pill below the subtitle
+function display_shav_product_rating_pill() {
+    global $post;
+    $product = wc_get_product($post->ID);
+    if (!$product)
+        return;
+    ?>
+    <div class="shav-product-rating-pill">
+        <span class="rating-count">+300K Opinii</span>
+        <span class="rating-sep" aria-hidden="true"></span>
+        <span class="rating-score">4.9</span>
+        <span class="rating-stars" aria-label="5 z 5 gwiazdek">
+            <?php for ($i = 0; $i < 5; $i++) : ?>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="#e9bd0b">
+                    <path d="M12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
+                </svg>
+            <?php endfor; ?>
+        </span>
+    </div>
+    <?php
+}
+add_action('woocommerce_single_product_summary', 'display_shav_product_rating_pill', 7);
+
 
 
 
